f4650b774923a0cc6b3baadb740600a5ea3945b7
App Finally a Bit <hubert.nashenda@gmail.com>
2016-07-22 15:32:19 +0000

    updated route.db

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
   auth_hash = request.env['omniauth.auth']
 
   render :text => auth_hash.inspect
+    
+  @products = ShopifyAPI::Product.all
+		import_new_customers
+		import_new_products
+		update_product_inventory  
+    
   end
 
   def failure
