aaa32c6d1d83a43b16978b282cc9470b31bf8710
App Finally a Bit <hubert.nashenda@gmail.com>
2016-07-22 13:23:33 +0000

    beyond authorization 3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
+SCOPE = 'read_products,read_orders,read_customers,write_shipping'
 
-  provider :shopify, "613ab5bbe4133b6c004ec13676f9cbb9", "64b5df6d0f4c4a183a9fe646cbf0b73f", :scope => 'read_products,read_orders,read_customers,write_shipping'
+#SHOPIFY_API_KEY = ENV['SHOPIFY_API_KEY']
+#SHOPIFY_SHARED_SECRET = ENV['SHOPIFY_SHARED_SECRET']
+SHOPIFY_API_KEY="613ab5bbe4133b6c004ec13676f9cbb9"
+SHOPIFY_SHARED_SECRET ="64b5df6d0f4c4a183a9fe646cbf0b73f"
+  
+  
+  provider :shopify, SHOPIFY_API_KEY, SHOPIFY_SHARED_SECRET, :scope => SCOPE
 
 
 
