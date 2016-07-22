1fb93800e657180a07dc72b4c10b63acaf40221c
App Finally a Bit <hubert.nashenda@gmail.com>
2016-07-22 13:00:34 +0000

    beyond authorization 3

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-
-
-  provider :shopify, "613ab5bbe4133b6c004ec13676f9cbb9", "64b5df6d0f4c4a183a9fe646cbf0b73f", :scope => 'read_products,read_orders,read_customers,write_shipping'
-
-
-
-end
