7ebed0140b8b36a6a6d1ed9aa399aafe6450d22e
App Finally a Bit <hubert.nashenda@gmail.com>
2016-07-23 00:02:33 +0000

    token -session-shop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
   auth_hash = request.env['omniauth.auth']
-   shop_session = request.env['omniauth.auth'].uid
+   @shop_session = request.env['omniauth.auth'].uid
   #render :text => auth_hash.inspect
 
 
