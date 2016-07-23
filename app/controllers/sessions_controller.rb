d3b2ba0398792ba2ca568161022417c81856747e
App Finally a Bit <hubert.nashenda@gmail.com>
2016-07-23 00:06:57 +0000

    token -session-shop

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,8 @@ class SessionsController < ApplicationController
   def create
   auth_hash = request.env['omniauth.auth']
    @shop_session = request.env['omniauth.auth'].uid
+    @token = request.env['omniauth.auth']['credentials']['token']
+    #@session =ShopifyAPI::Session.new(@shop_session, @token)
   #render :text => auth_hash.inspect
 
 
